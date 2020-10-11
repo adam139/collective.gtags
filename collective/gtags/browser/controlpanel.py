fb3c15ccd0c4d26e9dacaf905fe413180ff57e1c
adam139 <yuejun.tang@gmail.com>
2020-10-11 22:36:55 +0800

    add project_tags field widget custom

--- collective/gtags/browser/controlpanel.py
+++ collective/gtags/browser/controlpanel.py
@@ -17,7 +17,8 @@ class TagSettingsEditForm(controlpanel.RegistryEditForm):
     
     def updateFields(self):
         super(TagSettingsEditForm, self).updateFields()
-        self.fields['tags'].widgetFactory = TextLinesFieldWidget
+        self.fields['tags'].widgetFactory = TextLinesFieldWidget 
+        self.fields['project_tags'].widgetFactory = TextLinesFieldWidget
         self.fields['unique_categories'].widgetFactory = TextLinesFieldWidget
         self.fields['required_categories'].widgetFactory = TextLinesFieldWidget
     
@@ -25,6 +26,8 @@ class TagSettingsEditForm(controlpanel.RegistryEditForm):
         super(TagSettingsEditForm, self).updateWidgets()
         self.widgets['tags'].rows = 8
         self.widgets['tags'].style = u'width: 30%;'
+        self.widgets['project_tags'].rows = 8
+        self.widgets['project_tags'].style = u'width: 30%;'
         self.widgets['unique_categories'].rows = 8
         self.widgets['unique_categories'].style = u'width: 30%;'
         self.widgets['required_categories'].rows = 8
